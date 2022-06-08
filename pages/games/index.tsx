2e54b3ea469c4e949ad3093af51151d552fd9217
MetalTurtle <dekolis1218@gmail.com>
2022-06-08 16:40:07 -0700

    Add games and players pages

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,8 +1,19 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from "../_layout";
 import styles from "../../styles/utils.module.sass";
+import { collections, connectToDatabase } from "../../services/database.service";
+import { jsonify } from "../../lib/util";
+import Game from "../../models/game";
+import { Alert, ListGroup } from "react-bootstrap";
+import Link from "next/link";
+import dayjs from "dayjs";
 
-const games = () => {
+type Props = {
+    error: boolean
+    games: Game[]
+}
+
+const games = ({error, games}: Props) => {
     return (
         <Layout>
             <Head>
@@ -10,13 +21,38 @@ const games = () => {
             </Head>
             <h1 className={ styles.headingLg }>Games</h1>
             <h2 className={ styles.headingMd }>This is the games page</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.</p>
+            { error ?
+                <Alert variant="danger">There was an error loading the games!</Alert> :
+                <ListGroup variant="flush">{
+                    games.map(game => (
+                        <Link href={ "/games/" + game._id }>
+                            <ListGroup.Item key={ game._id.toString() } action as="a">
+                                { dayjs(game.date).format("MMM D, YYYY") }
+                            </ListGroup.Item>
+                        </Link>
+                    ))
+                }</ListGroup>
+            }
         </Layout>
     )
 }
 
 export default games
+
+export async function getServerSideProps() {
+    try {
+        await connectToDatabase()
+
+        const games = await collections.games.find({}).toArray() as Game[]
+
+        return {
+            props: {
+                error: !games,
+                games: jsonify(games),
+            }
+        }
+    } catch (e) {
+        console.error(e)
+        return { props: { error: true } }
+    }
+}
--- pages/players/index.tsx
+++ pages/players/index.tsx
@@ -1,8 +1,18 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from "../_layout";
 import styles from "../../styles/utils.module.sass";
+import { collections, connectToDatabase } from "../../services/database.service";
+import Player from "../../models/player";
+import { jsonify } from "../../lib/util";
+import { Alert, ListGroup } from "react-bootstrap";
+import Link from "next/link";
 
-const players = () => {
+type Props = {
+    error: boolean
+    players: Player[]
+}
+
+const players = ({ error, players }: Props) => {
     return (
         <Layout>
             <Head>
@@ -10,13 +20,38 @@ const players = () => {
             </Head>
             <h1 className={ styles.headingLg }>Players</h1>
             <h2 className={ styles.headingMd }>This is the players page</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.</p>
+            { error ?
+                <Alert variant="danger">There was an error loading the players!</Alert> :
+                <ListGroup variant="flush">{
+                    players.map(player => (
+                        <Link href={ "/players/" + player._id }>
+                            <ListGroup.Item key={ player._id.toString() } action as="a">
+                                { player.name }
+                            </ListGroup.Item>
+                        </Link>
+                    ))
+                }</ListGroup>
+            }
         </Layout>
     )
 }
 
 export default players
+
+export async function getServerSideProps() {
+    try {
+        await connectToDatabase()
+
+        const players = await collections.players.find({}).toArray() as Player[]
+
+        return {
+            props: {
+                error: !players,
+                players: jsonify(players),
+            }
+        }
+    } catch (e) {
+        console.error(e)
+        return { props: { error: true } }
+    }
+}
